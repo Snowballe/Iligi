4ebea09138fdba4152435cd85f4761b4e1344b72
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-06-22 15:17:56 +0200

    adding commercial notice to footer

--- Views/Components/footer.php
+++ Views/Components/footer.php
@@ -1,21 +1,32 @@
 <footer class="fixed-bottom">
     <div class="bg-light">
         <div class="container">
-        <hr class="mb-3">
+            <hr class="mb-3">
             <div class="row">
-                
+
 
                 <div class="col ">
                     <ul class="list-inline text-center">
-                        <li class="list-inline-item"><a class="text-decoration-none" href="about.php">À propos</a></li>
+                        <li class="list-inline-item "><a class="text-decoration-none" href="about.php">À propos</a></li>
                         <li class="list-inline-item">|</li>
                         <li class="list-inline-item"><a class="text-decoration-none" href="cookies.php">Vie Privée</a></li>
                         <li class="list-inline-item">|</li>
-                        <li class="list-inline-item"><a class="text-decoration-none" href="legalNotice.php">Mentions légales</a></li>
+                        <li class="list-inline-item"><?php
+                                                        $db = dbConnect();
+
+                                                        $getTexts = $db->prepare('SELECT commercial_notice FROM admin_user_notice WHERE id_notice=1');
+
+                                                        $getTexts->execute() or die(print_r($getText->errorInfo()));
+                                                        foreach($getTexts as $getText){
+                                                            echo("<div style=\"width:40rem;\" class=\"badge bg-light text-dark text-wrap\">".$getText['commercial_notice']."</div>");
+                                                        }
+                                                        ?></li>
                         <li class="list-inline-item">|</li>
                         <li class="list-inline-item"><a class="text-decoration-none" href="termsOfUse">Conditions d'utilisation</a></li>
                         <li class="list-inline-item">|</li>
-                        <a target="_blank" href="https://getbootstrap.com/"><li class="list-inline-item"><img src="Assets/icons/icons/bootstrap.svg" width="30" height="30" title="Bootstrap" alt="Bootstrap SVG"></li></a>
+                        <a target="_blank" href="https://getbootstrap.com/">
+                            <li class="list-inline-item"><img src="Assets/icons/icons/bootstrap.svg" width="30" height="30" title="Bootstrap" alt="Bootstrap SVG"></li>
+                        </a>
                     </ul>
                 </div>
             </div>
@@ -30,11 +41,11 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <script>
-$(document).ready(function(){
-    if( $('#identity_pieces_landlord').is(':empty')){
-        $('#identity_pieces_landlord').print();
-    };
-});
+    $(document).ready(function() {
+        if ($('#identity_pieces_landlord').is(':empty')) {
+            $('#identity_pieces_landlord').print();
+        };
+    });
 </script>
 <!-- <script> -->
 <!-- function confirmDeleteIdPieces(){
